8463a8fee7e8927aa5fc8b2e8c78e3ee2262b410
danbuis <dan.buis88@gmail.com>
2019-08-26 20:45:14 -0400

    Changed text of home page to be more exciting

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,29 @@ export default () => <div>
 <Header />
     <div className = {css.landing_page}>
         <h2>Andromeda</h2>
-        <p>Andromeda is a game that takes place in the Andromeda Galaxy shortly after humanity has discovered faster than light travel
-            A number of corporations have pooled their resources to build a large warp rail facility in the Andromeda Galaxy to ship materials
-            back to the Milky Way.
+        <p>Several centuries from now humanity has developed the faster than light drive and spread
+            through much of the Milky Way Galaxy. For over a century humanity has been at war with various
+             extraterrestrials as it struggles to expand its territory. As resources were consumed at ever 
+             increasing rates it became evident that if humanity desired to continue this expansion, or to 
+             even hold its ground, new technologies would be required.
         </p>
-        <p>This website serves to document the various craftable items that can be used by the player.  Users can compare similar items to see how they differ
-            and can see what the requirements are to craft items.
+        <p>Enter the intergalactic warp rail. Warp rail technology was about 2 generations old, and widely
+             regarded as commonplace. It catapulted a ship several lightyears nearly instantaneously, trading 
+             speed for a bit of inaccuracy for the shift back to real space. Several mega corporations worked 
+             together to build a warp rail several orders of magnitude larger than the ones used for conventional
+             travel. This warp rail was intended to send small amounts of material to the nearby Andromeda Galaxy 
+             to build a return warp rail, and the infrastructure needed to send supplies back to the Milk Way.     
+        </p>
+        <p>You have made the trip from the Milky Way and have just arrived at the Andromeda Galaxy hoping to make
+            a name for yourself. A whole new galaxy awaits with vast opportunities and exciting adventures to be had. 
+            Perhaps you will help maintain humanities foothold by ferrying vital supplies to outposts and the few military
+             fronts that have developed. Perhaps you will mine resources to be made into useful equipment such as ships, ammo,
+             or the components to build items. Perhaps you will work with one of the corporations who built the 
+             warp rail to build their items and develop new cutting edge variations of their current catalog.
+        </p>
+        <h2>The choice is yours.</h2>
+        <p>This website serves as a repository of items in the game, how they interact, the skills that you can master,
+             and the corporations that you will interact with in the Andromeda Galaxy.
         </p>
     </div>
 </div>
